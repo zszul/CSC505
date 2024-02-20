4c8aaf55954b48b2751633d670f1297f6309e6a8
zszul <150448992+zszul@users.noreply.github.com>
2024-02-20 12:11:03 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,31 @@ public class Main {
     
         String ticketT = myObj.nextLine();  // Read user input
         System.out.println("ticket type is: " + ticketT);  // Output user input
+                String username = "a"; // change this later to a scanner input
+
+    	int longQueue = queueLength(250,350);
+    	
+    	int shortQueue = queueLength(20,40);
+    	
+    	int mediumQueue = queueLength(100,150);
+        AQueue<String> concert = new AQueue<String>(longQueue); 
+        AQueue<String> theatre = new AQueue<String>(shortQueue);
+        AQueue<String> sports = new AQueue<String>(mediumQueue);
+        
+        if (ticketT == "concert") {
+        	concert.enqueue(username);
+        }
+        if (ticketT == "theatre") {
+        	theatre.enqueue(username);
+        }
+        if (ticketT == "sports") {
+        	sports.enqueue(username);
+        }
+        
+      }
+    public static int queueLength(int min, int max) { // randomizing queue length
+        return (int) (Math.random() * (max - min + 1)) + min;
+    }
       }
       //purchase
       //
