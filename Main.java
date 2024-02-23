93d384bd90e38ec89f30c0d36f249370fa6f1357
zszul <150448992+zszul@users.noreply.github.com>
2024-02-23 09:18:52 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,8 @@ public class Main {
     
         String ticketT = myObj.nextLine();  // Read user input
         System.out.println("ticket type is: " + ticketT);  // Output user input
-                String username = "a"; // change this later to a scanner input
+        
+        String username = "a"; // change this later to a scanner input
 
     	int longQueue = queueLength(250,350);
     	
@@ -18,25 +19,41 @@ public class Main {
         AQueue<String> theatre = new AQueue<String>(shortQueue);
         AQueue<String> sports = new AQueue<String>(mediumQueue);
         
+        Ticket sportsTickets = new Ticket();
+        int sportsAmount = Ticket.getRandomArbitrary(80,130);
+        
+        Ticket concertTickets = new Ticket();
+        int concertAmount = Ticket.getRandomArbitrary(220, 320);
+        
+        Ticket theatreTickets = new Ticket();
+        int theatreAmount = Ticket.getRandomArbitrary(10,30);
+        
+        
         if (ticketT == "concert") {
+        	for (int i = 0; i < longQueue; i++) {
+        		concert.enqueue("a");
+        	}
         	concert.enqueue(username);
         }
         if (ticketT == "theatre") {
+        	for (int i = 0; i < shortQueue; i++) {
+        		theatre.enqueue("a");
+        	}
         	theatre.enqueue(username);
         }
         if (ticketT == "sports") {
+        	for (int i  = 0; i < mediumQueue; i++) {
+        		sports.enqueue("a");
+        	}
         	sports.enqueue(username);
         }
         
+        
+        
+      
       }
     public static int queueLength(int min, int max) { // randomizing queue length
         return (int) (Math.random() * (max - min + 1)) + min;
     }
-      }
-     
-      
-
-    }
-
 
 
