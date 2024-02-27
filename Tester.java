1fcccd93ff70bd8ce519cf1902818c3547b764c5
zszul <150448992+zszul@users.noreply.github.com>
2024-02-26 21:06:49 -0500

    Delete Tester.java

--- Tester.java
+++
@@ -1,6 +0,0 @@
-public class Tester {
-    public static void main(String[] args){
-        Tickets SmallAvailableNumOfTicket = new Tickets();
-            System.out.println(SmallAvailableNumOfTicket.getRandomArbitrary(30, 55));
-    }
-}
