712a2274cbd302070ab5620b81a0f8f1aba1b443
congbrianna <159942349+congbrianna@users.noreply.github.com>
2024-02-27 18:34:34 -0500

    Update TicketTimer.java

--- TicketTimer.java
+++ TicketTimer.java
@@ -1,22 +1,23 @@
+import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-import javax.swing.Timer;
-
 public class TicketTimer implements ActionListener {
 
-	private Timer t;
-	Queue<String> concert;
-	Queue<String> theatre;
-	Queue<String> sports;
+	Timer t;
+	AQueue<String> concert;
+	AQueue<String> theatre;
+	AQueue<String> sports;
 	int concertAmount;
 	int theatreAmount;
 	int sportsAmount;
-	String ticketT;
+	int sportButtonClickedNumber;
+	int theatreButtonClickedNumber;
+	int concertButtonClickedNumber;
+	int timeElapsed;
 
 
-	public TicketTimer(AQueue<String> c, AQueue<String> th, AQueue<String> s, int concertA, int theatreA, int sportsA, String ticketT) {
+	public TicketTimer(AQueue<String> c, AQueue<String> th, AQueue<String> s, int concertA, int theatreA, int sportsA, int concertClicks, int theatreClicks, int sportClicks) {
 		t = new Timer(2000, this);
 		concert = c;
 		theatre = th;
@@ -24,32 +25,35 @@ public class TicketTimer implements ActionListener {
 		concertAmount = concertA;
 		theatreAmount = theatreA;
 		sportsAmount = sportsA;
+		sportButtonClickedNumber = sportClicks;
+		concertButtonClickedNumber = concertClicks;
+		theatreButtonClickedNumber = theatreClicks;
+		timeElapsed = 0;
 
+        t.start();
 	}
+
 	public static int random(int min, int max) { // randomizing queue length
 		return (int) (Math.random() * (max - min + 1)) + min;
 	}
 
-
-
 	public void actionPerformed(ActionEvent e) {
-
-		for (int i = 0; i < random(1,5); i++) {
-			if (ticketT == "concert") {
-				concert.dequeue();
-				concertAmount--;
-			}
-			if (ticketT == "theatre") {
-				theatre.dequeue();
-				theatreAmount--;
-			}
-			if (ticketT == "sports") {
-				sports.dequeue();
-				sportsAmount--;
-			}
-		}
-
+		for (int i = 0; i < random(1, 5); i++) {
+            if (concertButtonClickedNumber >= 2) {
+                concert.dequeue();
+                concertAmount--;
+            }
+            if (theatreButtonClickedNumber >= 2) {
+                theatre.dequeue();
+                theatreAmount--;
+            }
+            if (sportButtonClickedNumber >= 2) {
+                sports.dequeue();
+                sportsAmount--;
+            }
+        }
 	}
+	
 	public int getConcert() {
 		return concertAmount;
 	}
@@ -59,5 +63,4 @@ public class TicketTimer implements ActionListener {
 	public int getSports() {
 		return sportsAmount;
 	}
-
 }
